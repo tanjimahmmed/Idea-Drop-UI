6429c9936ec2df33bcbb85ded5a3bdd1e0590841
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-09-09 18:22:02 +0600

    add refresh token when it expires

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { getStoredAccessToken } from "./authToken";
+import { getStoredAccessToken, setStoredAccessToken } from "./authToken";
+import { refreshAccessToken } from "@/api/auth";
 
 const api = axios.create({
     baseURL: '/api',
@@ -9,6 +10,7 @@ const api = axios.create({
     }
 });
 
+// Attach token on refresh
 api.interceptors.request.use((config) => {
     const token = getStoredAccessToken();
     if(token){
@@ -16,6 +18,32 @@ api.interceptors.request.use((config) => {
     }
 
     return config;
-})
+});
+
+// Refresh token after expire
+api.interceptors.response.use(
+    (res) => res,
+    async(error) => {
+        const originalRequest = error.config;
+
+        if(error.response?.status === 401 && 
+            !originalRequest._retry && 
+            !originalRequest.url.includes('/auth/refresh')) 
+        {
+            originalRequest._retry = true;
+
+            try {
+                const {accessToken: newToken} = await refreshAccessToken();
+                setStoredAccessToken(newToken);
+                originalRequest.headers.Authorization = `Bearer ${newToken}`;
+                return api(originalRequest)
+            } catch(err) {
+                console.log('Refresh token failed', err)
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
 
 export default api;
