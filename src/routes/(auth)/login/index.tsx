452d834ce3ec6e85fe4aecf137675e62af89ae81
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-09-09 15:34:50 +0600

    add login authentication and add conditional nav links

--- src/routes/(auth)/login/index.tsx
+++ src/routes/(auth)/login/index.tsx
@@ -1,4 +1,7 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute, Link, useNavigate } from '@tanstack/react-router'
+import {useMutation} from '@tanstack/react-query';
+import { loginUser } from '@/api/auth';
+import { useAuth } from '@/context/AuthContext';
 import { useState } from 'react'
 
 export const Route = createFileRoute('/(auth)/login/')({
@@ -6,12 +9,36 @@ export const Route = createFileRoute('/(auth)/login/')({
 })
 
 function LoginPage() {
+  const navigate = useNavigate();
+  const {setAccessToken, setUser} = useAuth();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const {mutateAsync, isPending} = useMutation({
+    mutationFn: loginUser,
+    onSuccess: (data) => {
+      setAccessToken(data.accessToken);
+      setUser(data.user);
+      navigate({to: '/ideas'})
+    },
+    onError: (err: any) => {
+      setError(err.message)
+    }
+  });
+
+  const handleSubmit = async (e:React.FormEvent) => {
+    e.preventDefault();
+    setError('');
+    await mutateAsync({email, password})
+  }
 
   return (<div className='max-w-md mx-auto'>
     <h1 className='text-3xl font-bold mb-6'>Login</h1>
-    <form className="space-y-4">
+    {error && (
+      <div className='bg-red-100 text-red-700 px-4 py-2 rounded mb-4'>{error}</div>
+    )}
+    <form onSubmit={handleSubmit} className="space-y-4">
 
       <input type="email" className='w-full border border-gray rounded-md p-2' 
       placeholder='Email'
@@ -23,8 +50,9 @@ function LoginPage() {
       value={password}
       onChange={(e) => setPassword(e.target.value)} autoComplete='off'/>
 
-      <button className='bg-blue-600 text-white font-semibold px-4 py-2 rounded-md w-full disabled:opacity-50'>
-        Login
+      <button className='bg-blue-600 text-white font-semibold px-4 py-2 rounded-md w-full disabled:opacity-50'
+      disabled={isPending}>
+        {isPending ? 'Logging In...' : 'Login'}
       </button>
     </form>
 
--- src/routes/(auth)/register/index.tsx
+++ src/routes/(auth)/register/index.tsx
@@ -1,18 +1,48 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute, Link, useNavigate } from '@tanstack/react-router'
 import { useState } from 'react'
+import {useMutation} from '@tanstack/react-query';
+import { registerUser } from '@/api/auth';
+import { useAuth } from '@/context/AuthContext';
 
 export const Route = createFileRoute('/(auth)/register/')({
   component: RegisterPage,
 })
 
 function RegisterPage() {
+  const navigate = useNavigate();
+  const {setAccessToken, setUser} = useAuth();
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const {mutateAsync, isPending} = useMutation({
+    mutationFn: registerUser,
+    onSuccess: (data) => {
+      setAccessToken(data.accessToken)
+      setUser(data.user);
+      navigate({to: '/ideas'})
+    },
+    onError: (err: any) => {
+      setError(err.message);
+    }
+  });
+
+  const handleSubmit = async(e: React.FormEvent) => {
+    e.preventDefault();
+    try{
+      await mutateAsync({name, email, password})
+    }catch(err: any){
+      console.log(err.message)
+    }
+  }
 
   return (<div className='max-w-md mx-auto'>
     <h1 className='text-3xl font-bold mb-6'>Register</h1>
-    <form className="space-y-4">
+    {error && (
+      <div className='bg-red-100 text-red-700 px-4 py-2 rounded mb-4'>{error}</div>
+    )}
+    <form onSubmit={handleSubmit} className="space-y-4">
       
       <input type="text" className='w-full border border-gray rounded-md p-2' 
       placeholder='Name'
@@ -29,8 +59,9 @@ function RegisterPage() {
       value={password}
       onChange={(e) => setPassword(e.target.value)} autoComplete='off'/>
 
-      <button className='bg-blue-600 text-white font-semibold px-4 py-2 rounded-md w-full disabled:opacity-50'>
-        Register
+      <button className='bg-blue-600 text-white font-semibold px-4 py-2 rounded-md w-full disabled:opacity-50'
+      disabled={isPending}>
+        {isPending ? 'Registering...' : 'Register'}
       </button>
     </form>
 
