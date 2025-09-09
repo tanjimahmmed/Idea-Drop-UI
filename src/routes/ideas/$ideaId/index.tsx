543e48e9716f83b71c740ff0507ba99fee64db1d
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-09-09 18:26:26 +0600

    add hide controls

--- src/routes/ideas/$ideaId/index.tsx
+++ src/routes/ideas/$ideaId/index.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute, Link, useNavigate } from '@tanstack/react-router';
 import {queryOptions, useSuspenseQuery, useMutation} from '@tanstack/react-query';
 import { fetchIdea, deleteIdea } from '@/api/ideas';
+import { useAuth } from '@/context/AuthContext';
 
 
 const ideaQueryOptions = (ideaId: string) => queryOptions({
@@ -20,6 +21,8 @@ function IdeaDetailsPage() {
   const {data: idea} = useSuspenseQuery(ideaQueryOptions(ideaId));
 
   const navigate = useNavigate();
+  
+  const {user} = useAuth();
 
   const {mutateAsync: deleteMutate, isPending} = useMutation({
     mutationFn: () => deleteIdea(ideaId),
@@ -43,16 +46,23 @@ function IdeaDetailsPage() {
     </Link>
     <h2 className='text-2xl font-bold'>{idea.title}</h2>
     <p className='mt-2'>{idea.description}</p>
-    {/* Edit Link */}
-    <Link to='/ideas/$ideaId/edit' 
-    params={{ideaId}}
-    className='inline-block text-sm bg-yellow-500 hover:bg-yellow-600 text-white mt-4 mr-2 px-4 py-2 rounded transition'
-    >
-      Edit
-    </Link>
-    {/* Delete Button */}
-    <button onClick={handleDelete} disabled={isPending} className='text-sm bg-red-600 hover:bg-red-700 text-white mt-4 px-4 py-2 rounded transition disabled:opacity:50'>
-      {isPending ? 'Deleting...' : 'Delete'}
-      </button>
+
+    {user && user.id === idea.user && (
+      <>
+        {/* Edit Link */}
+      <Link to='/ideas/$ideaId/edit' 
+      params={{ideaId}}
+      className='inline-block text-sm bg-yellow-500 hover:bg-yellow-600 text-white mt-4 mr-2 px-4 py-2 rounded transition'
+      >
+        Edit
+      </Link>
+      {/* Delete Button */}
+      <button onClick={handleDelete} disabled={isPending} className='text-sm bg-red-600 hover:bg-red-700 text-white mt-4 px-4 py-2 rounded transition disabled:opacity:50'>
+        {isPending ? 'Deleting...' : 'Delete'}
+        </button>
+      </>
+    )}
+
+    
   </div>
 }
